890872c267dbdc371645e0483f316ed7b0ff4ca0
uuutsav <21051903@kiit.ac.in>
2024-09-11 23:21:45 +0530

    Refactor - Navbar UI change. Migrate to TailwindCSS

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import './Navbar.css';
 
-const Navbar = ({loggedIn, onLogout}) => {
+const Navbar = ({ loggedIn, onLogout }) => {
   return (
-    <nav className="navbar">
-      <div className="logo">
-        <img src="path_to_your_logo.png" alt="Logo" className="logoImg" />
+    <nav className="h-[7vh] p-3 bg-gray-200 flex justify-between items-center lg:gap-52 ">
+      <div id='left' className="left w-1/5 text-2xl cursor-pointer ">
+        {/* <img src="path_to_your_logo.png" alt="Logo" className="logoImg" /> */}
+        DoubtCom
       </div>
-      <div className="links">
-        <a href="/messages" className="link">Messages</a>
+      <div id='middle' className="middle hidden w-3/5 md:flex justify-around ">
+        <a href="#" className="border-2 duration-150 hover:scale-110 hover:border-b-green-300  ">Home</a>
+        <a href="#" className="border-2 duration-150 hover:scale-110 hover:border-b-green-300">Feed</a>
+        <a href="#" className="border-2 duration-150 hover:scale-110 hover:border-b-green-300">About</a>
+        <a href="#" className="border-2 duration-150 hover:scale-110 hover:border-b-green-300">Contact</a>
       </div>
-      <div className="logout">
-        <button className="logoutButton" onClick={onLogout}>Logout</button>
+      <div id='right' className="right hidden w-1/3 md:flex gap-3 justify-end ">
+        <button className="bg-green-300 px-5 py-3 rounded-full hover:scale-110 duration-150" onClick={onLogout}>Logout</button>
+        <div id='nav-search' className='flex rounded-full overflow-hidden duration-150 hover:scale-105'  >
+          <input type="text" placeholder='Search' className='p-3' />
+        </div>
+      </div>
+      <div id="hamburger" className='md:hidden '>
+        Hamburger
       </div>
     </nav>
   );
