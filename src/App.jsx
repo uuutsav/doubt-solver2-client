4db959190fcd61e4630c9378fdecb3219709c6e1
uuutsav <21051903@kiit.ac.in>
2024-09-14 17:17:07 +0530

    Refactor - Clear clutter in main.jsx, move auth logic from main.jsx to useAuth custom hook.
    main.jsx is for rendering the root component and setting root level wrappers like BrowserRouter and RecoilRoot.

--- src/App.jsx
+++ src/App.jsx
@@ -5,50 +5,55 @@ import Home from './components/Home'
 import Login from './components/Login'
 import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import Signup from './components/Signup'
+import { jwtDecode } from 'jwt-decode'
+import { RecoilRoot, useRecoilValue, useSetRecoilState } from 'recoil'
+import Layout from './components/Layout'
+import Loader from './components/Loader/Loader'
+import ProtectedRoute from './routes/ProtectedRoute'
+import { authAtom } from './recoil/atoms/authAtom'
 
 function App() {
-  const [username, setUsername] = useState('')
-  // const [password, setPassword] = useState('')
-  const [token, setToken] = useState(localStorage.getItem('token'))
-  // const navigate = useNavigate();
+  const setAuthState = useSetRecoilState(authAtom);
 
   useEffect(() => {
+    // Token expiry check
+    isTokenValid ? "" : setAuthState({isTokenValid: false, user: null, token: null});
+  })
+
+  const isTokenValid = () => {
     const token = localStorage.getItem('token');
-    // if(token) {
-    //   navigate('/home');
-    // }
-  }, []);
+    console.log("Checking token ")
+    if (!token) {
+      console.warn("No auth token found")
+      return false;
+    }
 
-  const handleLogin = (token) => {
-    setToken(token);
-    // Store the token in localStorage
-    localStorage.setItem('token', token);
-    // Assuming you have a way to get the username from the token
-    setUsername('username_from_token');
+    try {
+      const decodedToken = jwtDecode(token);
+      const currentTime = Date.now() / 1000; // Convert milliseconds to seconds
+      if (decodedToken.exp && decodedToken.exp < currentTime) {
+        // Token is expired
+        return false;
+      }
+      return true;
+    } catch (error) {
+      console.error('Error decoding or validating token:', error);
+      return false;
+    }
   };
 
-  const handleLogout = () => {
-    setToken('');
-    // Clear the token from localStorage
-    localStorage.removeItem('token');
-    setUsername('');
-  };
-
-  const handleSignup = (token) => {
-    setToken(token);
-    localStorage.setItem('token', token);
-    // Assuming you have a way to get the username from the token
-    setUsername('username_from_token');
-  };
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={token ? <Navigate to="/home" /> : <Login  />} />
-        <Route path="/home" element={token ? <Home username={username} onLogout={handleLogout} /> : <Navigate to="/" />} />
-        <Route path="/signup" element={token ? <Navigate to="/home" /> : <Signup onSignup={handleSignup} />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route element={<ProtectedRoute />} >
+        <Route path='/home' element={<Home />} />
+
+        {/* Wrong route  */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Route>
+    </Routes>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,67 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Layout from './components/Layout.jsx'
-import Home from './components/Home.jsx'
-import Loader from './components/Loader/Loader.jsx'
-import Login from './components/Login.jsx'
-import {jwtDecode} from 'jwt-decode';
-import Signup from './components/Signup.jsx';
-
-
-const isTokenValid = () => {
-  console.log("Checking ")
-  const token = localStorage.getItem('token');
-  // Validate token - exist or not , expired or not
-  if (!token) {
-    // Token does not exist
-    return false;
-  }
-
-  try {
-    const decodedToken = jwtDecode(token);
-    // console.log("Decoded JWT: ", decodedToken)
-    const currentTime = Date.now() / 1000; // Convert milliseconds to seconds
-    if (decodedToken.exp && decodedToken.exp < currentTime) {
-      // Token is expired
-      return false;
-    }
-
-    // Hehe token good
-    return true;
-  } catch (error) {
-    console.error('Error decoding or validating token:', error);
-    return false;
-  }
-
-  // return !!token; // Returns true if token exists, false otherwise
-};
-
-// ProtectedRoute component for routes that require authentication
-const ProtectedRoute = ({ element, ...rest }) => {
-  return isTokenValid() ? (
-    element
-  ) : (
-    <Navigate to="/login" replace />
-  );
-};
+import { RecoilRoot } from 'recoil';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route element={<ProtectedRoute element={<Layout />} />}>
-          <Route index element={<Home />} />
-          {/* Use ProtectedRoute for protected routes */}
-          <Route path="home" element={<ProtectedRoute element={<Home />} />} />
-          <Route path="test" element={<ProtectedRoute element={<Loader />} />} />
-          {/* Add more protected routes as needed */}
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Route>
-      </Routes>
-    </Router>
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <BrowserRouter>
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  </BrowserRouter>
+  // </React.StrictMode>,
 );
